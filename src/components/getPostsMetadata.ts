2ca67cddd2a22f3912b25d6524f15457885f5473
asd234 <anujdube0123@gmail.com>
2023-03-13 11:07:29 +0530

    fix

--- src/components/getPostsMetadata.ts
+++ src/components/getPostsMetadata.ts
@@ -3,12 +3,12 @@ import fs from 'fs';
 import matter from 'gray-matter';
 
 const getPostMetaData = (): postMetadata[] => {
-  const folder = 'src/posts/';
+  const folder = 'posts/';
   const files = fs.readdirSync(folder);
   const markdownFiles = files.filter((file) => file.endsWith('.md'));
 
   const posts = markdownFiles.map((fileName) => {
-    const fileContents = fs.readFileSync(`src/posts/${fileName}`, 'utf-8');
+    const fileContents = fs.readFileSync(`posts/${fileName}`, 'utf-8');
     const matterResult = matter(fileContents);
     return {
       title: matterResult.data.title,
@@ -17,6 +17,7 @@ const getPostMetaData = (): postMetadata[] => {
       slug: fileName.replace('.md', ''),
     };
   });
+  console.log(__dirname);
   return posts;
 };
 
